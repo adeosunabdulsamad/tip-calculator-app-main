e352afe06d9401748b8e032e5e47aa32220f1436
Adeosun Abdulsamad <adeosunabdulsamad25@gmail.com>
2025-07-20 18:02:54 +0100

    Fix tip calculation and formatting for individual amounts

--- index.js
+++ index.js
@@ -4,14 +4,29 @@ let people = document.getElementById("number-people");
 let tipAmountPerson = document.getElementById("tip-amount-person");
 let totalAmountPerson = document.getElementById("total-amount-person");
 let tipPercentage = document.getElementById("custom"); 
+let stringAmountPerPerson = "0.00";
+let stringTotalAmountPerson = "0.00";
 tipPercentage.addEventListener("keypress", function(event) 
 {
     if (event.key === "Enter") {
-        event.preventDefault;
+        event.preventDefault();
         let tipPercentageValue = tipPercentage.value/100;
         let tipAmountPerPerson = bill.value * tipPercentageValue/people.value;
-        let stringAmountPerPerson = tipAmountPerPerson.toFixed(2);
+        let totalamountPerPerson = bill.value * (1+tipPercentageValue)/people.value;
+        if (isFinite(tipAmountPerPerson)) {
+            stringAmountPerPerson = tipAmountPerPerson.toFixed(2);
+        }
+        else {
+            stringAmountPerPerson = "0.00"
+        };
+        if (isFinite(totalamountPerPerson)) {
+            stringTotalAmountPerson = totalamountPerPerson.toFixed(2);
+        }
+        else {
+            stringTotalAmountPerson = "0.00"
+        };
         tipAmountPerson.innerHTML = "$" + stringAmountPerPerson;
+        totalAmountPerson.innerHTML = "$" + stringTotalAmountPerson;
     }
 });
 
@@ -25,20 +40,46 @@ tipPercentage.addEventListener("keypress", function(event)
 bill.addEventListener("keypress", function(event) 
 {
     if (event.key === "Enter") {
-        event.preventDefault;
+        event.preventDefault();
         let tipPercentageValue = tipPercentage.value/100;
         let tipAmountPerPerson = bill.value * tipPercentageValue/people.value;
-        let stringAmountPerPerson = tipAmountPerPerson.toFixed(2);
+        let totalamountPerPerson = bill.value * (1+tipPercentageValue)/people.value;
+        if (isFinite(tipAmountPerPerson)) {
+            stringAmountPerPerson = tipAmountPerPerson.toFixed(2);
+        }
+        else {
+            stringAmountPerPerson = "0.00"
+        };
+        if (isFinite(totalamountPerPerson)) {
+            stringTotalAmountPerson = totalamountPerPerson.toFixed(2);
+        }
+        else {
+            stringTotalAmountPerson = "0.00"
+        };
         tipAmountPerson.innerHTML = "$" + stringAmountPerPerson;
+        totalAmountPerson.innerHTML = "$" + stringTotalAmountPerson;
     }
 });
 people.addEventListener("keypress", function(event) 
 {
     if (event.key === "Enter") {
-        event.preventDefault;
+        event.preventDefault();
         let tipPercentageValue = tipPercentage.value/100;
         let tipAmountPerPerson = bill.value * tipPercentageValue/people.value;
-        let stringAmountPerPerson = tipAmountPerPerson.toFixed(2);
+        let totalamountPerPerson = bill.value * (1+tipPercentageValue)/people.value;
+        if (isFinite(tipAmountPerPerson)) {
+            stringAmountPerPerson = tipAmountPerPerson.toFixed(2);
+        }
+        else {
+            stringAmountPerPerson = "0.00"
+        };
+        if (isFinite(totalamountPerPerson)) {
+            stringTotalAmountPerson = totalamountPerPerson.toFixed(2);
+        }
+        else {
+            stringTotalAmountPerson = "0.00"
+        };
         tipAmountPerson.innerHTML = "$" + stringAmountPerPerson;
+        totalAmountPerson.innerHTML = "$" + stringTotalAmountPerson;
     }
 });
