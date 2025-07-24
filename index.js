96f8ea297f82dc97d01989db9940e2c5010ff6a1
Adeosun Abdulsamad <adeosunabdulsamad25@gmail.com>
2025-07-24 14:40:20 +0100

    made the tip calculations real time using the input event listener

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 let bill = document.getElementById('number-bill');
 let people = document.getElementById('number-people');
-let tipAmountPerson = document.getElementById("tip-amount-person");
-let totalAmountPerson = document.getElementById("total-amount-person");
-let tipPercentage = document.getElementById("custom"); 
-let stringAmountPerPerson = "0.00";
-let stringTotalAmountPerson = "0.00";
-let resetButton = document.getElementById("button");
-const radios = document.querySelectorAll('input[type="radio"]');
+let tipAmountPerson = document.getElementById('tip-amount-person');
+let totalAmountPerson = document.getElementById('total-amount-person');
+let tipPercentage = document.getElementById('custom'); 
+let stringAmountPerPerson = '0.00';
+let stringTotalAmountPerson = '0.00';
+let resetButton = document.getElementById('button');
+const radios = document.querySelectorAll('.general');
+console.log(radios);
 const selectedRadio = Array.from(radios).find(radio => radio.checked);
 
 radios.forEach(radio => {radio.addEventListener('change', () => {
@@ -20,8 +21,8 @@ tipPercentage.addEventListener('input', () => {
     radios.forEach(radio => radio.checked = false);
     calculateTip();
 });
-bill.addEventListener('input', calculateTip());
-people.addEventListener('input', calculateTip());
+bill.addEventListener('input', calculateTip);
+people.addEventListener('input', calculateTip);
 
 resetButton.addEventListener('click', function ()
 {
@@ -30,8 +31,6 @@ resetButton.addEventListener('click', function ()
 });
 
 function calculateTip() {
-    let tipAmountPerPerson = bill.value * tipPercentageValue/people.value;
-    let totalamountPerPerson = bill.value * (1+tipPercentageValue)/people.value;
     const selectedRadio = Array.from(radios).find(radio => radio.checked);
     let tipPercentageValue
 
@@ -42,6 +41,8 @@ function calculateTip() {
     else if (tipPercentage.value !=='') {
         tipPercentageValue = tipPercentage.value/100;
     };
+    let tipAmountPerPerson = bill.value * tipPercentageValue/people.value;
+    let totalamountPerPerson = bill.value * (1+tipPercentageValue)/people.value;
 
     if (isFinite(tipAmountPerPerson)) {
         stringAmountPerPerson = tipAmountPerPerson.toFixed(2);
