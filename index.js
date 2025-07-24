972407f273c53716e3fa491219d3a09b59562c28
Adeosun Abdulsamad <adeosunabdulsamad25@gmail.com>
2025-07-24 14:23:28 +0100

    radio buttons and custominput no longer exist at the same time

--- index.js
+++ index.js
@@ -1,92 +1,60 @@
-let query = document.getElementsByName("query");
-let bill = document.getElementById("number-bill");
-let people = document.getElementById("number-people");
+let bill = document.getElementById('number-bill');
+let people = document.getElementById('number-people');
 let tipAmountPerson = document.getElementById("tip-amount-person");
 let totalAmountPerson = document.getElementById("total-amount-person");
 let tipPercentage = document.getElementById("custom"); 
 let stringAmountPerPerson = "0.00";
 let stringTotalAmountPerson = "0.00";
 let resetButton = document.getElementById("button");
-tipPercentage.addEventListener("keypress", function(event) 
-{
-    if (event.key === "Enter") {
-        event.preventDefault();
-        let tipPercentageValue = tipPercentage.value/100;
-        let tipAmountPerPerson = bill.value * tipPercentageValue/people.value;
-        let totalamountPerPerson = bill.value * (1+tipPercentageValue)/people.value;
-        if (isFinite(tipAmountPerPerson)) {
-            stringAmountPerPerson = tipAmountPerPerson.toFixed(2);
-        }
-        else {
-            stringAmountPerPerson = "0.00"
-        };
-        if (isFinite(totalamountPerPerson)) {
-            stringTotalAmountPerson = totalamountPerPerson.toFixed(2);
-        }
-        else {
-            stringTotalAmountPerson = "0.00"
-        };
-        tipAmountPerson.textContent = "$" + stringAmountPerPerson;
-        totalAmountPerson.textContent = "$" + stringTotalAmountPerson;
-    }
-});
+const radios = document.querySelectorAll('input[type="radio"]');
+const selectedRadio = Array.from(radios).find(radio => radio.checked);
 
-/*query.addEventListener("keypress", function(event){
-    if (event.key === "")
-        for (i = 0; i < bill.length; i++) {
-            if (bill[i].checked) {
-                console.log(tipPercent.value);
-                }
-}});*/
-bill.addEventListener("keypress", function(event) 
-{
-    if (event.key === "Enter") {
-        event.preventDefault();
-        let tipPercentageValue = tipPercentage.value/100;
-        let tipAmountPerPerson = bill.value * tipPercentageValue/people.value;
-        let totalamountPerPerson = bill.value * (1+tipPercentageValue)/people.value;
-        if (isFinite(tipAmountPerPerson)) {
-            stringAmountPerPerson = tipAmountPerPerson.toFixed(2);
-        }
-        else {
-            stringAmountPerPerson = "0.00"
-        };
-        if (isFinite(totalamountPerPerson)) {
-            stringTotalAmountPerson = totalamountPerPerson.toFixed(2);
-        }
-        else {
-            stringTotalAmountPerson = "0.00"
-        };
-        tipAmountPerson.textContent = "$" + stringAmountPerPerson;
-        totalAmountPerson.textContent = "$" + stringTotalAmountPerson;
+radios.forEach(radio => {radio.addEventListener('change', () => {
+    if (radio.checked) {
+        tipPercentage.value='';
+        calculateTip();
     }
 });
-people.addEventListener("keypress", function(event) 
-{
-    if (event.key === "Enter") {
-        event.preventDefault();
-        let tipPercentageValue = tipPercentage.value/100;
-        let tipAmountPerPerson = bill.value * tipPercentageValue/people.value;
-        let totalamountPerPerson = bill.value * (1+tipPercentageValue)/people.value;
-        if (isFinite(tipAmountPerPerson)) {
-            stringAmountPerPerson = tipAmountPerPerson.toFixed(2);
-        }
-        else {
-            stringAmountPerPerson = "0.00"
-        };
-        if (isFinite(totalamountPerPerson)) {
-            stringTotalAmountPerson = totalamountPerPerson.toFixed(2);
-        }
-        else {
-            stringTotalAmountPerson = "0.00"
-        };
-        tipAmountPerson.textContent = "$" + stringAmountPerPerson;
-        totalAmountPerson.textContent = "$" + stringTotalAmountPerson;
-    }
 });
+tipPercentage.addEventListener('input', () => {
+    radios.forEach(radio => radio.checked = false);
+    calculateTip();
+});
+bill.addEventListener('input', calculateTip());
+people.addEventListener('input', calculateTip());
 
 resetButton.addEventListener('click', function ()
 {
     tipAmountPerson.textContent="$0.00";
     totalAmountPerson.textContent="$0.00";
 });
+
+function calculateTip() {
+    let tipAmountPerPerson = bill.value * tipPercentageValue/people.value;
+    let totalamountPerPerson = bill.value * (1+tipPercentageValue)/people.value;
+    const selectedRadio = Array.from(radios).find(radio => radio.checked);
+    let tipPercentageValue
+
+    if (selectedRadio) {
+        tipPercentageValue = selectedRadio.value/100;
+        console.log(selectedRadio);
+    }
+    else if (tipPercentage.value !=='') {
+        tipPercentageValue = tipPercentage.value/100;
+    };
+
+    if (isFinite(tipAmountPerPerson)) {
+        stringAmountPerPerson = tipAmountPerPerson.toFixed(2);
+    }
+    else {
+        stringAmountPerPerson = "0.00"
+    };
+    if (isFinite(totalamountPerPerson)) {
+        stringTotalAmountPerson = totalamountPerPerson.toFixed(2);
+    }
+    else {
+        stringTotalAmountPerson = "0.00"
+    };
+    tipAmountPerson.textContent = "$" + stringAmountPerPerson;
+    totalAmountPerson.textContent = "$" + stringTotalAmountPerson;
+}
